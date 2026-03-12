1bb426f599e24ecfae64177641dacf2e5be4ec49
HyeongseoShin <tlsgudtj123@gmail.com>
2026-03-13 02:43:51 +0900

    [HOTSPOT-325] chore: LastWeekServiceImpl import문 추가

--- src/main/java/hotspot/batch/jobs/usage_aggregation/job/step/usage_metrics/service/impl/LastWeekUsageServiceImpl.java
+++ src/main/java/hotspot/batch/jobs/usage_aggregation/job/step/usage_metrics/service/impl/LastWeekUsageServiceImpl.java
@@ -4,6 +4,7 @@ import java.time.LocalDate;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 import org.springframework.stereotype.Service;
 
