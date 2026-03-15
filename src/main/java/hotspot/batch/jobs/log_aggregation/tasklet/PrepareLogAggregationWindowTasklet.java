e8166115d44f0ff11d2e046379dbf380b3f2b2a2
Jae <lijae3552@gmail.com>
2026-03-16 05:41:59 +0900

    [HOTSPOT-346] feat: 로그 집계 처리 윈도우(from/to applied_seq) 준비 tasklet 추가

---
+++ src/main/java/hotspot/batch/jobs/log_aggregation/tasklet/PrepareLogAggregationWindowTasklet.java
@@ -0,0 +1,38 @@
+package hotspot.batch.jobs.log_aggregation.tasklet;
+
+import hotspot.batch.jobs.log_aggregation.job.LogAggregationProjection;
+import hotspot.batch.jobs.log_aggregation.repository.LogAggregationRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.batch.core.scope.context.ChunkContext;
+import org.springframework.batch.core.step.StepContribution;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.infrastructure.repeat.RepeatStatus;
+import org.springframework.stereotype.Component;
+
+@Component
+public class PrepareLogAggregationWindowTasklet implements Tasklet {
+
+    private static final Logger log = LoggerFactory.getLogger(PrepareLogAggregationWindowTasklet.class);
+
+    private final LogAggregationRepository logAggregationRepository;
+
+    public PrepareLogAggregationWindowTasklet(LogAggregationRepository logAggregationRepository) {
+        this.logAggregationRepository = logAggregationRepository;
+    }
+
+    @Override
+    public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) {
+        long fromAppliedSeq =
+                logAggregationRepository.findLastAppliedSeq(LogAggregationProjection.SUB_USAGE_MONTHLY_AGGREGATE);
+        long toAppliedSeq = logAggregationRepository.findMaxAppliedSeq();
+
+        var executionContext = chunkContext.getStepContext().getStepExecution().getJobExecution().getExecutionContext();
+        executionContext.putLong(LogAggregationProjection.CTX_FROM_APPLIED_SEQ, fromAppliedSeq);
+        executionContext.putLong(LogAggregationProjection.CTX_TO_APPLIED_SEQ, toAppliedSeq);
+
+        log.info("Prepared aggregation window. projectionName={} fromAppliedSeq={} toAppliedSeq={}",
+                LogAggregationProjection.SUB_USAGE_MONTHLY_AGGREGATE, fromAppliedSeq, toAppliedSeq);
+        return RepeatStatus.FINISHED;
+    }
+}
